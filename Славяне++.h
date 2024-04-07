b4c1e00c32e674afa9c68aee15f175c732fa212c
Minicubicsgames <152804917+Minicubicsgames@users.noreply.github.com>
2024-04-07 21:58:15 +0500

    Add files via upload

---
+++ Славяне++.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include <iostream>
+#include <math.h>
+#include <Windows.h>
+#define импорт #include
+#define число int
+#define плот float
+#define строчка std::string
+#define молвитьСтроку std::cout
+#define закончитьСтроку std::endl
+#define Ящер void
+#define ИспепилениеЯщеров return
+#define помощь <<
+#define СлавянскийЯзык 	setlocale(LC_ALL, "RU")
+#define Русь main
+#define началоРодимойСкобочки {
+#define скобочкаРодимая }
+#define конец ;
+#define прибавитьКазну +
+#define уменьшитьЯщеров -
+#define разделЯщерскихЗемель :
+#define присвоить =
+#define проверить ==
+#define славянскийЗажимЯйцами *
+#define славянскаяМатематика cos
+#define ЯщерскаяМатематика sin
+#define подождатьПередБитвой Sleep(2000)
